e313731e2d77e5c867e750b39362d673376b2f99
Iowa Dave <35478224+IowaDave@users.noreply.github.com>
2022-03-13 21:08:34 -0700

    DS3231 Arduino version at 13Mar2022

--- DS3231_Read_Write_Registers/DS3231_Read_Write_Registers.ino
+++ DS3231_Read_Write_Registers/DS3231_Read_Write_Registers.ino
@@ -1,23 +1,49 @@
+/*
+ * DS3231_Read_Write_Registers.ino
+ * Copyright 2022 by David G. Sparks
+ * All Rights Reserved
+ * 
+ * Read data from the hardware registers of a DS3231 Real Time Clock, 
+ * and write data to it. Use only the "Wire" software library of the Arduino IDE, 
+ * but not any device-specific library.
+ *
+ * Display a register's value as a string of eight characters, '0' or '1'.
+ *
+ * LICENSE
+ *
+ * DS3231_Read_Write_Registers.ino ("this program") is free software: you can redistribute it 
+ * and/or modify it under the terms of the GNU General Public License as published by the 
+ * Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. 
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include <Wire.h>
 
 /*
  * Buffer and function for converting an 8-bit byte
- * into a string of bit characters.
+ * into a string of eight characters, '0' or '1'.
+ * Because sprintf() does not perform bitwise formatting.
  * 
- * Later I adapted this code segment into the
- * displayRegister() function, below.
+ * An adaptation of this code segment is used
+ * in the displayRegister() function, below.
  * 
-char bitString[9]; // one more than string length
-
-
-void byteToBinaryString ( char * buf, byte b ) {
-  for (int i = 7; i >= 0; i--) {
-    buf[i] = (b & 1) == 0  ? '0' : '1';
-    b >>= 1;
-  }
-  buf[8] = '\0'; // ensure null terminator
-}
-*/
+ *
+ * char bitString[9]; // one more than string length
+ *
+ * void byteToBinaryString ( char * buf, byte b ) {
+ *   for (int i = 7; i >= 0; i--) {
+ *     buf[i] = (b & 1) == 0  ? '0' : '1';
+ *     b >>= 1;
+ *   }
+ *   buf[8] = '\0'; // ensure null terminator
+ * }
+ */
 
 uint8_t getRegister ( uint8_t regAddress ) {
   uint8_t regValue;
@@ -32,8 +58,9 @@ uint8_t getRegister ( uint8_t regAddress ) {
 }
 
 /*
- * Display the value o given register
- * and return the value to the caller
+ * Display the value of a given register,
+ * preceded by a label string,
+ * and return that value to the caller
  */
 uint8_t displayRegister (char * label, uint8_t registerAddress) {
   // get the value from the register
@@ -52,11 +79,11 @@ uint8_t displayRegister (char * label, uint8_t registerAddress) {
     regString[i] = (regValue & 1) == 0 ? '0' : '1';
     regValue >>= 1;
   }
-  // display the label and then the string
+  // display the label on a new line and then the string
   Serial.println(); // blank line
-  Serial.print("  "); // two spaces from left edge
+  Serial.print("  "); // offset two spaces from left edge of window
   Serial.println(label);
-  Serial.print("  "); // two spaces from left edge
+  Serial.print("  "); // offset two spaces from left edge of window
   Serial.println(regString);
 
   // return the original register value
@@ -130,18 +157,16 @@ void setup() {
   // randomly set new value for Alarm 2 minutes
   uint8_t newMin;
   
-  /* Arduino Method
+  /* Arduino Method*/
   // seed the rng
-  // randomSeed(analogRead(A0));
+  randomSeed(analogRead(A0));
   // get a new, different value
-  //while ((newMin = (uint8_t) random(0,12)) 
-  //      == originalA2Minutes) { } // try again
-  */
-  
+  while ((newMin = (uint8_t) random(0,12)) 
+        == originalA2Minutes) { } // try again
   /*
    * ESP 8266 method
+   * newMin = ESP.random() % 12; // 0 .. 11
    */
-  newMin = ESP.random() % 12; // 0 .. 11
 
  
   // set the new value
